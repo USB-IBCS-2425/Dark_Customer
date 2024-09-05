613d149601b30e85669dfdb8a43735959071e630
Brianvu7836 <112970264+Brianvu7836@users.noreply.github.com>
2024-09-05 14:05:08 -0700

    uploadind notes

---
+++ notes9_5.java
@@ -0,0 +1,36 @@
+class notes9_5{
+	public static void main(String[] args) {
+		
+		//data types
+		// defined an integer variable with a value of 4
+		int x = 4;
+		x = 6;
+
+		System.out.println(x);
+
+		float y = (float) x; 
+
+		System.out.println(y);
+
+		String favColor = "red";
+		System.out.println(favColor);
+		System.out.println(favColor + "" + x);
+
+		//operators built in to java
+		//+ - * / % ++ --
+
+		int z = 10;
+		z = z++;
+		z = z+1;
+		// z = currently 12
+		z = z%2;
+		// 12/2 take the remainder which is 0
+
+
+
+
+
+	}
+
+
+ }
