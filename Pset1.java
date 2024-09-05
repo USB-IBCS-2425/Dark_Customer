c74b88add06ff89681205fd4bfa34cc414cfaf4b
Brianvu7836 <112970264+Brianvu7836@users.noreply.github.com>
2024-09-05 14:09:47 -0700

    uploadind notes

---
+++ Pset1.java
@@ -0,0 +1,20 @@
+class Pset1 {
+	public static void main(String[] args) {
+		
+		System.out.print("Brian Vu is a student in IB CS\n\n");
+		System.out.print(" The different between Java and Python is Python is a Interpreter which translates and execute at the same time, execute code line by line (Python) however Java is a Compiler translate everything first then you executes it, compiles code into bytecode\n\n");		
+		System.out.print(" High level language is more abstract, using natural language(English-like statements) for operations but assemble languages is using mnemonic codes for operations it's like a non-natural language, machine language that dependent\n\n");
+		System.out.print("General purpose is write once, read it every time, anywhere, it will work the same with any devices without changing any codes\n\n");
+
+
+
+
+
+	}
+
+
+
+
+
+
+}
