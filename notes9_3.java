0c8df9d889ce8170452e0ce51bc19761c50a15b6
Brianvu7836 <112970264+Brianvu7836@users.noreply.github.com>
2024-09-05 14:07:42 -0700

    uploadind notes

---
+++ notes9_3.java
@@ -0,0 +1,20 @@
+class notes9_3 { 
+	// main methods require the public
+	// key notation to allow access from outside
+	//the module
+	// static means that the method itself runs the same
+	//way every time it is accessed
+	// void is the return value
+	// nothing (void) is returned
+	// main is the name of the method(always lowercase main)
+	// String[] args is the parameter
+	//{} define the area for the main method
+	public static void main(String[] args) {
+		// this is inside (defining) the main
+		//method
+		System.out.print("ni hao");
+	}
+
+
+
+}
